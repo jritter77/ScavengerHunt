b9ba672f02909795b1ad03c7bc294515ab7ac352
jordan <jordanurbany@gmail.com>
2022-10-13 16:29:59 -0700

    just pushing initial commit for profile

--- App/views/profile/ChangeCredentials.js
+++ App/views/profile/ChangeCredentials.js
@@ -1,12 +1,52 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, StyleSheet} from 'react-native'
 import React from 'react'
 
+
 const ChangeCredentials = ({navigation}) => {
+  
+  const [password, setPassword] = React.useState('');
+  const [confirmpassword, setConfirmPassword] = React.useState('');
+  const [username, setNewUsername] = React.useState('');
+  const [newpassword, setNewPassword] = React.useState('');
+ 
+
+ 
+ 
   return (
-    <View>
-      <Text>ChangeCredentials</Text>
+    <View style = {styles.text}>
+    
+      <TextInput 
+        onChangeText = {setPassword}
+        placeholder= 'Password'
+        
+      />
+      <TextInput 
+        onChangeText = {setConfirmPassword}
+        placeholder= 'Confirm Password'
+       
+      />
+      <TextInput 
+        onChangeText = {setNewUsername}
+        placeholder= 'Change Username'
+        
+      />
+      <TextInput 
+        onChangeText = {setNewPassword}
+        placeholder = 'Change Password'
+        
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  text:{
+   width: '50%',
+   font: 40,
+   alignContent: 'center'
+
+  }
+
+})
 
 export default ChangeCredentials
