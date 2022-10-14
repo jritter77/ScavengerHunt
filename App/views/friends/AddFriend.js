7742415d05b9b45141b6e0c14a27a5f040ebb8dd
Antonio Quintanilla <antonioq1999@gmail.com>
2022-10-13 17:06:10 -0700

    Initial commit

--- App/views/friends/AddFriend.js
+++ App/views/friends/AddFriend.js
@@ -1,12 +1,28 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, StyleSheet } from 'react-native'
 import React from 'react'
 
+
 const AddFriend = ({navigation}) => {
+  const [user, setUser] = React.useState('')
+
+function handleSubmit() {
+  // Verify the username exists
+
+  // If not notify user
+
+  // Else send friend request
+}
   return (
     <View>
-      <Text>AddFriend</Text>
+      <Text>Add Friend</Text>
+      <TextInput
+      onChangeText = {setUser}
+      placeholder = "Friend Username"
+      />
     </View>
   )
 }
+
+// const styles = StyleSheet.create
 
 export default AddFriend
