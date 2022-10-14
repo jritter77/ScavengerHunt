323f0deffdb15109b93135446df21435f371df8b
jordan <jordanurbany@gmail.com>
2022-10-13 19:41:29 -0700

    revert

--- App/views/profile/ChangeCredentials.js
+++ App/views/profile/ChangeCredentials.js
@@ -1,52 +1,13 @@
-import { View, Text, TextInput, StyleSheet} from 'react-native'
+import { View, Text } from 'react-native'
 import React from 'react'
-
+import Styles from '../../Styles'
 
 const ChangeCredentials = ({navigation}) => {
-  
-  const [password, setPassword] = React.useState('');
-  const [confirmpassword, setConfirmPassword] = React.useState('');
-  const [username, setNewUsername] = React.useState('');
-  const [newpassword, setNewPassword] = React.useState('');
- 
-
- 
- 
   return (
-    <View style = {styles.text}>
-    
-      <TextInput 
-        onChangeText = {setPassword}
-        placeholder= 'Password'
-        
-      />
-      <TextInput 
-        onChangeText = {setConfirmPassword}
-        placeholder= 'Confirm Password'
-       
-      />
-      <TextInput 
-        onChangeText = {setNewUsername}
-        placeholder= 'Change Username'
-        
-      />
-      <TextInput 
-        onChangeText = {setNewPassword}
-        placeholder = 'Change Password'
-        
-      />
+    <View style={Styles.StandardStyles.page}>
+      <Text>ChangeCredentials</Text>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  text:{
-   width: '50%',
-   font: 40,
-   alignContent: 'center'
-
-  }
-
-})
 
 export default ChangeCredentials
