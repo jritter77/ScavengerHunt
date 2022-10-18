98f39884029d9b772616f188dab82cc5b5684f05
Jake Ritter <jakeritter77@gmail.com>
2022-10-17 20:01:05 -0700

    -

--- App/App.js
+++ App/App.js
@@ -1,69 +1,56 @@
-import 'react-native-gesture-handler';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator, DrawerToggleButton } from '@react-navigation/drawer';
-import React from 'react';
+import "react-native-gesture-handler";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  createDrawerNavigator,
+  DrawerToggleButton,
+} from "@react-navigation/drawer";
+import React from "react";
 
+import Dashboard from "./views/dashboard/Dashboard";
 
-import Dashboard from './views/dashboard/Dashboard';
-
-
-import ProfileStack from './views/profile/Index.js';
-import HuntStack from './views/hunts/Index.js';
-import FriendStack from './views/friends/Index.js';
-import LoginStack from './views/loginSignup/Index.js';
-import { LinearGradient } from 'expo-linear-gradient';
-import Styles from './Styles';
-
-
+import ProfileStack from "./views/profile";
+import HuntStack from "./views/hunts";
+import FriendStack from "./views/friends";
+import LoginStack from "./views/loginSignup";
+import { LinearGradient } from "expo-linear-gradient";
+import Styles from "./Styles";
 
 const Drawer = createDrawerNavigator();
 
-
-
 export default function App() {
-
-  const [userId, setUserId] = React.useState('');
+  const [userId, setUserId] = React.useState("");
 
   const [loggedIn, setLoggedIn] = React.useState(false);
-
-
-
 
   if (loggedIn) {
     console.log(userId);
     return (
       <NavigationContainer>
-        <Drawer.Navigator initialRouteName='Dashboard' screenOptions={Styles.NavHeaderStyle} >
-            <Drawer.Screen 
-              name='Dashboard'
-              component={Dashboard}
-            />
-            <Drawer.Screen 
-              name='ProfileStack'
-              component={ProfileStack}
-              options={{drawerLabel: 'Profile'}}
-            />
-            <Drawer.Screen 
-              name='HuntStack'
-              component={HuntStack}
-              options={{drawerLabel: 'Scavenger Hunts'}}
-            />
-            <Drawer.Screen 
-              name='FriendStack'
-              component={FriendStack}
-              options={{drawerLabel: 'Friends'}}
-            />
+        <Drawer.Navigator
+          initialRouteName="Dashboard"
+          screenOptions={Styles.NavHeaderStyle}
+        >
+          <Drawer.Screen name="Dashboard" component={Dashboard} />
+          <Drawer.Screen
+            name="ProfileStack"
+            component={ProfileStack}
+            options={{ drawerLabel: "Profile" }}
+          />
+          <Drawer.Screen
+            name="HuntStack"
+            component={HuntStack}
+            options={{ drawerLabel: "Scavenger Hunts" }}
+          />
+          <Drawer.Screen
+            name="FriendStack"
+            component={FriendStack}
+            options={{ drawerLabel: "Friends" }}
+          />
         </Drawer.Navigator>
       </NavigationContainer>
     );
+  } else {
+    return <LoginStack setLoggedIn={setLoggedIn} setUserId={setUserId} />;
   }
-  else {
-    return (
-        <LoginStack setLoggedIn={setLoggedIn} setUserId={setUserId}/>
-    )
-  }
-  
 }
-
-
