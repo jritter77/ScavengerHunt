ee1f1dd7312b1abe4e4989cb28d1c7daa7c76afa
Antonio Quintanilla <antonioq1999@gmail.com>
2022-10-18 22:45:25 -0700

    added submit button to AddFriend page

--- App/views/friends/AddFriend.js
+++ App/views/friends/AddFriend.js
@@ -1,29 +1,41 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native'
+import { View, Text, TextInput, StyleSheet} from 'react-native'
 import React from 'react'
 import Styles from '../../Styles'
-
+import StandardButton from '../../components/StandardButton'
 
 const AddFriend = ({navigation}) => {
   const [user, setUser] = React.useState('')
 
-function handleSubmit() {
-  // Verify the username exists
+  function handleSubmit() {
+    // Verify the username exists
+    console.log('Pressed');
+    // If not notify user
 
-  // If not notify user
-
-  // Else send friend request
-}
+    // Else send friend request
+  }
   return (
-    <View>
-      <Text>Add Friend</Text>
+    <View style ={Styles.StandardStyles.page}>
+      <Text style={styles.Text}>Add Friend</Text>
       <TextInput
-      placeholder = "Friend Username"
+      placeholder = 'Friend Username'
       onChangeText = {setUser}
       />
+      <StandardButton
+      title = 'Submit'
+      onPress = {handleSubmit}
+      />
+
     </View>
   )
 }
 
-// const styles = StyleSheet.create
+const styles = StyleSheet.create({
+    Text: {
+      fontSize: '20pt'
+    }
+  })
+
+
+
 
 export default AddFriend
