86512e571c839b0f258d188bf9bd05ebcf01eca2
jritter77 <jr550@humboldt.edu>
2022-10-19 20:29:40 -0700

    - merge complete

--- App/views/hunts/MyHunts.js
+++ App/views/hunts/MyHunts.js
@@ -42,14 +42,24 @@ const MyHunts = ({navigation}) => {
   
 
   return (
-    <View style={Styles.StandardStyles.page}>
+    <ScrollView style={styles.scrollContainer} contentContainerStyle={styles.scrollContainerContent}>
+      <TextInput 
+        style={styles.search}
+        onChangeText={setSearchTerm}
+        placeholder={'Search'}
+      />
       <StandardButton 
-          title='Active Hunt'
-          onPress={() => {
-            navigation.navigate('ActiveHunt')
-          }}
-        />
-    </View>
+          title='Search'
+          onPress={() => handleSubmit()}
+      />
+      <View style={styles.huntsContainer}>
+        {populateHunts()}
+      </View>
+      <StandardButton 
+        title={'Show More'}
+        onPress={() => console.log('Show More!')}
+      />
+    </ScrollView>
   )
 }
 
