b4018a8c47ec0972cbdc757a66d3127262130620
Antonio Quintanilla <antonioq1999@gmail.com>
2022-11-13 10:25:04 -0800

    - styles for LocalFriends.js

--- App/components/LocalFriends.js
+++ App/components/LocalFriends.js
@@ -1,13 +1,38 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { TouchableHighlight } from 'react-native-gesture-handler'
 
 const LocalFriends = ({id, username}) => {
   return (
-   <TouchableHighlight>
-    <Text>{id}</Text>
+   <TouchableHighlight style={styles.container}>
+    <Text style={styles.user}>{username}</Text>
+    <View></View>
    </TouchableHighlight>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      width: '70%',
+      marginBottom: '5%',
+      backgroundColor: 'white'
+  },
+
+  user: {
+    borderWidth: 2,
+    borderRadius: 5,
+    width: '80%',
+    fontSize: 20,
+    marginBottom: '5%',
+    textAlign: 'center',
+    padding: '5%',
+    fontWeight: 'bold'
+    
+
+  }
+})
 
 export default LocalFriends
