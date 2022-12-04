e36b7fec4b6a95d23f79e8c241e18fe67165dead
Jake Ritter <jakeritter77@gmail.com>
2022-12-04 10:48:07 -0800

    - making current

--- App/components/Rating.js
+++ App/components/Rating.js
@@ -1,49 +1,58 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
 
-const Rating = ({rating, size, backgroundColor, style}) => {
-
-  let percent = (1 - rating/5) * 100;
-
+const Rating = ({ rating, size, backgroundColor, style }) => {
+  let percent = (1 - rating / 5) * 100;
 
   if (!rating) {
     return (
-      <View style={{...styles.container, ...style}}>
-        <Text style={{fontSize: size, ...styles.text}}>No&nbsp;Rating</Text>
-      </View>)
+      <View style={{ ...styles.container, ...style }}>
+        <Text style={{ fontSize: size, ...styles.text }}>No&nbsp;Rating</Text>
+      </View>
+    );
   }
 
   return (
-    <View style={{...styles.container, ...style}}>
-      <Text style={{fontSize: size, ...styles.text}}>{rating}</Text>
+    <View style={{ ...styles.container, ...style }}>
+      <Text style={{ fontSize: size, ...styles.text }}>
+        {rating.toFixed(1)}
+      </Text>
       <View>
-        <Text style={{fontSize: size, ...styles.stars}}>&#9733;&#9733;&#9733;&#9733;&#9733;</Text>
-        <View style={{ width: `${percent}%`, backgroundColor: backgroundColor, ...styles.cover}}></View>
+        <Text style={{ fontSize: size, ...styles.stars }}>
+          &#9733;&#9733;&#9733;&#9733;&#9733;
+        </Text>
+        <View
+          style={{
+            width: `${percent}%`,
+            backgroundColor: backgroundColor,
+            ...styles.cover,
+          }}
+        ></View>
       </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'inherit',
-    marginBottom: '5%',
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center'
+    backgroundColor: "inherit",
+    marginBottom: "5%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
   },
   stars: {
-    color: 'blue'
+    color: "blue",
   },
   cover: {
-    position: 'absolute',
-    height: '100%',
-    right: '0%',
-    overflow: 'hidden'
+    position: "absolute",
+    height: "100%",
+    right: "0%",
+    overflow: "hidden",
   },
   text: {
-    marginRight: '5%'
-  }
-})
+    marginRight: "5%",
+  },
+});
 
-export default Rating
+export default Rating;
